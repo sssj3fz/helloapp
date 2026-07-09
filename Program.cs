199e7adbd414fc2408766d28898fd273d66b1424
black <black@example.com>
2026-07-09 19:31:54 +0800

    Call SayHelloWhite and SayHelloBlack after greeting

--- Program.cs
+++ Program.cs
@@ -5,15 +5,22 @@ class Program
     static void Main(string[] args)
     {
         SayHello();
+        SayHelloWhite();
+        SayHelloBlack();
     }
 
-    static void SayHelloBlack()
-{
-    Console.WriteLine("hello black");
+    static void SayHello()
+    {
+        Console.WriteLine("hello everyone");
     }
 
     static void SayHelloWhite()
     {
         Console.WriteLine("hello white");
+    }
+
+    static void SayHelloBlack()
+    {
+        Console.WriteLine("hello black");
     }
 }
